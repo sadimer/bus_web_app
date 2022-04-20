febe1b8c0bf404b1f9340612cbc783dc41ae78f1
sadimer <roman.stolyarov@mail.ru>
2022-04-21 00:24:07 +0300

    Add login

--- application/src/main/java/app/config/WebConfig.java
+++ application/src/main/java/app/config/WebConfig.java
@@ -1,12 +1,15 @@
 package app.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
@@ -15,9 +18,25 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 @ComponentScan(basePackages = "app.controllers")
 public class WebConfig implements WebMvcConfigurer {
     @Bean
+    public InternalResourceViewResolver resolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/templates/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
+
+    @Bean
     public ClassLoaderTemplateResolver yourTemplateResolver() {
         ClassLoaderTemplateResolver configurer = new ClassLoaderTemplateResolver();
-        configurer.setPrefix("templates/");
+        configurer.setPrefix("/templates/");
         configurer.setSuffix(".html");
         configurer.setTemplateMode(TemplateMode.HTML);
         configurer.setCharacterEncoding("UTF-8");
--- application/src/main/java/app/controllers/WebController.java
+++ application/src/main/java/app/controllers/WebController.java
@@ -1,37 +1,55 @@
 package app.controllers;
 
 import DAO.*;
+import com.google.common.collect.Lists;
 import entities.*;
-import DTO.*;
-import mapper.*;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
+
 @Controller
 public class WebController {
     @RequestMapping(value = "/home", method = RequestMethod.GET)
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
+        modelAndView.setViewName("home.html");
         return modelAndView;
     }
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public ModelAndView profile(@RequestParam(name = "user_id", required = true) Long user_id) {
-        UsersDAO users = new UsersDAO();
-        Users entity = users.getEntityById(user_id, Users.class);
+    @RequestMapping(value = "/signin", method = RequestMethod.GET)
+    public ModelAndView signin() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("profile");
-        modelAndView.addObject("user_id", user_id);
-        modelAndView.addObject("is_admin", entity.getAdmin());
-        modelAndView.addObject("name", entity.getName());
-        modelAndView.addObject("login", entity.getLogin());
-        modelAndView.addObject("email", entity.getEmail());
-        modelAndView.addObject("phone", entity.getPhone());
+        modelAndView.setViewName("signin.html");
+        return modelAndView;
+    }
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public ModelAndView login(@RequestParam(name = "user_login", required = true) String user_login,
+                              @RequestParam(name = "user_psw", required = true) String user_psw,
+                              @RequestParam(name = "error", required = false) String error) {
+        UsersDAO users = new UsersDAO();
+        List<Users> result = users.filter(Map.of("loginFilter",
+                Lists.newArrayList(user_login)), Users.class);
+        ModelAndView modelAndView = new ModelAndView();
+        if (result.size() == 0) {
+            modelAndView.setViewName("signin.html");
+            modelAndView.addObject("error", "User not found!");
+            return modelAndView;
+        } else if (result.size() > 1) {
+            modelAndView.setViewName("signin.html");
+            modelAndView.addObject("error", "Multiple logins found!");
+            return modelAndView;
+        }
+        if (!result.get(0).getPasswd().equals(user_psw)) {
+            modelAndView.setViewName("signin.html");
+            modelAndView.addObject("error", "Wrong password!");
+            return modelAndView;
+        }
+        modelAndView.setViewName("signin.html");
+        modelAndView.addObject("user_login", user_login);
         return modelAndView;
     }
 }
